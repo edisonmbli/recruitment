4d32721fd0cee0a5ee5316a2152154ad26596ca9
edisonmbli <edisonmbli@gmail.com>
2021-03-10 17:42:45 +0800

    recruitment, up to candidate application part 1

---
+++ jobs/models.py
@@ -0,0 +1,48 @@
+from django.utils import timezone
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+# 候选人学历
+DEGREE_TYPE = ((u'本科', u'本科'), (u'硕士', u'硕士'), (u'博士', u'博士'))
+
+JobTypes = [
+    (0, "技术类"),
+    (1, "产品类"),
+    (2, "运营类"),
+    (3, "设计类"),
+]
+
+Cities = [
+    (0, "北京"),
+    (1, "上海"),
+    (2, "广州"),
+    (3, "深圳"),
+]
+
+
+class Job(models.Model):
+    job_type = models.SmallIntegerField(
+        blank=False, choices=JobTypes, verbose_name="职位类别", default=0)
+
+    job_name = models.CharField(
+        max_length=250, blank=False, verbose_name="职位名称", default='填写职位名称...')
+
+    job_city = models.SmallIntegerField(
+        choices=Cities, blank=False, verbose_name="工作地点", default=0)
+
+    job_responsibility = models.TextField(
+        max_length=1024, verbose_name="职位职责", default='填写职责描述...')
+
+    job_requirement = models.TextField(
+        max_length=1024, blank=False, verbose_name="职位要求", default='填写职位要求描述...')
+
+    creator = models.ForeignKey(
+        User, verbose_name="创建人", null=True, on_delete=models.SET_NULL)
+
+    created_date = models.DateTimeField(
+        verbose_name="创建日期", default=timezone.now)
+
+    modified_date = models.DateTimeField(
+        verbose_name="修改时间", default=timezone.now)
