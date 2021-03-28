bedd7f62f36e767da408df6eb3efb53b8aef8962
edisonmbli <edisonmbli@gmail.com>
2021-03-28 10:48:58 +0800

    course 31-32 add middleware

---
+++ interview/performance.py
@@ -0,0 +1,21 @@
+import time
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def performance_logger_middleware(get_response):
+    def middleware(request):
+        # before view handling
+        start_time = time.time()
+
+        response = get_response(request)
+
+        # after view handling
+        duration = time.time() - start_time
+        response["X-Page-Duration-ms"] = int(duration * 1000)
+        logger.info('%s %s %s', duration, request.path, request.GET.dict())
+
+        return response
+
+    return middleware
